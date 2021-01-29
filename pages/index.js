5e8ca4d166310a428a5a03590911ec42617e327c
FLAVIO NASCIMENTO <fnascimento00@gmail.com>
2021-01-29 10:20:09 -0300

    Ajustes da Aula 3

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,14 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import styled from 'styled-components';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export default function Home() {
   const router = useRouter();
@@ -40,8 +31,13 @@ export default function Home() {
             <p>{db.description}</p>
 
             <form onSubmit={(event) => Play(event)}>
-              <input placeholder="Digite seu Nome" onChange={(event) => setName(event.target.value)} />
-              <button type="submit" disabled={name.length === 0}>JOGAR</button>
+              <Input
+                name="nomeDoJogador"
+                placeholder="Digite seu Nome"
+                onChange={(event) => setName(event.target.value)}
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>JOGAR</Button>
             </form>
           </Widget.Content>
         </Widget>
